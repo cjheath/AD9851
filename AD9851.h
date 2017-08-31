32d1b5094acf912dacfcb05ef943e11079e79019
Clifford Heath <clifford.heath@gmail.com>
2017-08-31 15:47:04 +1000

    Implement reciprocal division that maximises accuracy with any core clock

--- AD9851.h
+++ AD9851.h
@@ -25,23 +25,23 @@
 #error "Arduino 1.6.0 or later (SPI library) is required"
 #endif
 
-#define	MAX_CORE_32	((01ULL<<29)<<32)	// MAX_CORE_32/core_freq fits in 32 bits
-
 template <
-	uint8_t ResetPin,		// Reset pin (active = high)
-	uint8_t FQ_UDPin,		// Data Load pin (active = pulse high)
-	uint32_t reference_freq = 180L*1000*1000,	// 180MHz
-	long SPIRate = 2000000,		// 2MBit/s
-	uint8_t SPIClkPin = 13,		// Note: do not change the SPI pins, they're currently ignored.
-	uint8_t SPIMOSIPin = 11
+  uint8_t ResetPin,                     // Reset pin (active = high)
+  uint8_t FQ_UDPin,                     // Data Load pin (active = pulse high)
+  uint32_t reference_freq = 180L*1000*1000, // 180MHz
+  long SPIRate = 2000000,               // 2MBit/s
+  uint8_t SPIClkPin = 13,               // Note: do not change the SPI pins, they're currently ignored.
+  uint8_t SPIMOSIPin = 11
 >
 class AD9851 {
-  uint32_t	reciprocal;
+  uint32_t      reciprocal;
+  uint8_t       shift;                  // (2<<shift) < core_clock <= (2<<shift+1) (27 for 180MHz)
+
 public:
   AD9851()
   {
-    pinMode(ResetPin, OUTPUT);	// Ensure we can reset the AD9851
-    pinMode(FQ_UDPin, OUTPUT);	// This control signal applies the loaded values
+    pinMode(ResetPin, OUTPUT);          // Ensure we can reset the AD9851
+    pinMode(FQ_UDPin, OUTPUT);          // This control signal applies the loaded values
     // Ensure that the SPI device is initialised
     // "setting SCK, MOSI, and SS to outputs, pulling SCK and MOSI low, and SS high"
     SPI.begin();
@@ -51,8 +51,8 @@ public:
 
   void reset()
   {
-    pulse(ResetPin);		// (minimum 5 cycles of the 30MHz clock)
-    pulse(SPIClkPin);		// Enable serial loading mode:
+    pulse(ResetPin);                    // (minimum 5 cycles of the 30MHz clock)
+    pulse(SPIClkPin);                   // Enable serial loading mode:
     pulse(FQ_UDPin);
 
     // To avoid possible false configuration settings, immediately set the frequency
@@ -63,14 +63,18 @@ public:
   void setClock(int32_t calibration = 0)
   {
     uint32_t core_clock = reference_freq * (1000000000ULL-calibration) / 1000000000ULL;
+    uint64_t    scaled = core_clock;
+    for (shift = 32; shift > 0 && (scaled&0x100000000ULL) == 0; shift--)
+        scaled <<= 1;                   // Ensure that reciprocal fits in 32 bits
     // The AVR gcc implementation has a 32x32->64 widening multiply.
     // This is quite accurate enough, and considerably faster than full 64x64.
-    reciprocal = MAX_CORE_32 / core_clock;
+    reciprocal = (0x1ULL<<(32+shift)) / core_clock;
   }
 
   uint32_t frequencyDelta(uint32_t freq) const
   {
-    return ((uint64_t)freq * reciprocal) / (0x1ULL<<29);
+    // The reciprocal/16 is a rounding factor determined experimentally
+    return ((uint64_t)freq * reciprocal + reciprocal/16) >> shift;
   }
 
   void setFrequency(uint32_t freq)
@@ -82,11 +86,11 @@ public:
   {
     SPI.beginTransaction(SPISettings(SPIRate, LSBFIRST, SPI_MODE0));
     for (int b = 0; b < 4; b++, tuning_word >>= 8)
-	    SPI.transfer(tuning_word&0xFF); 
+            SPI.transfer(tuning_word&0xFF); 
     // The last byte contains configuration settings including phase:
     SPI.transfer(0x01);
 
-    pulse(FQ_UDPin);		// Transfer the 40-bit control word into the DDS core
+    pulse(FQ_UDPin);                    // Transfer the 40-bit control word into the DDS core
     SPI.endTransaction();
   }
 
