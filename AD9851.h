b616aaae1768fe45e8e3e88210e8693f54af9983
Clifford Heath <clifford.heath@gmail.com>
2019-06-26 14:36:58 +1000

    Revised API and documentation

--- AD9851.h
+++ AD9851.h
@@ -8,7 +8,7 @@
  * Your chosen Reset pin to AD9851 ResetPin (device reset)
  *
  * To calibrate, use a zero calibration value, set some frequency and
- * measure it as accurately as you can with a frequency counter.
+ * measure it with a frequency counter.
  * Convert that value to parts-per-billion error (positive if the
  * frequency is high, negative if low) and provide that integer to
  * setClock() as the calibration value instead.
@@ -25,17 +25,33 @@
 #error "Arduino 1.6.0 or later (SPI library) is required"
 #endif
 
+#if     defined(DDS_MAX_PRECISION)
+#if     !defined(MAX_U64)
+#define MAX_U64 ((uint64_t)~0LL)
+#endif
+#endif
+
+// First four bytes are a delta word, LSB first.
+// The fifth byte configures the device:
+#define	AD9851_ENABLE_MULTIPLIER	0x01
+#define	AD9851_POWER_DOWN		0x04
+#define	AD9851_PHASE_MASK		0xF8
+#define	AD9851_PHASE_SHIFT		3
+
 template <
   uint8_t ResetPin,                     // Reset pin (active = high)
   uint8_t FQ_UDPin,                     // Data Load pin (active = pulse high)
-  uint32_t reference_freq = 180L*1000*1000, // 180MHz
+  uint32_t core_clock = 180L*1000*1000, // 180MHz; core clock before calibration
   long SPIRate = 2000000,               // 2MBit/s
-  uint8_t SPIClkPin = 13,               // Note: do not change the SPI pins, they're currently ignored.
-  uint8_t SPIMOSIPin = 11
+  uint8_t SPIClkPin = 13               // Note: do not change the SPI pins, they're currently ignored.
 >
 class AD9851 {
-  uint32_t      reciprocal;
-  uint8_t       shift;                  // (2<<shift) < core_clock <= (2<<shift+1) (27 for 180MHz)
+#if     defined(DDS_MAX_PRECISION)
+  uint64_t              reciprocal;     // (2^64-1)/calibrated_clock
+#else
+  uint32_t              reciprocal;     // 2^(64-shift)/calibrated_clock
+  uint8_t               shift;          // (2<<shift) < calibrated_clock <= (2<<shift+1) (27 for 180MHz)
+#endif
 
 public:
   AD9851()
@@ -55,26 +71,35 @@ public:
     pulse(SPIClkPin);                   // Enable serial loading mode:
     pulse(FQ_UDPin);
 
-    // To avoid possible false configuration settings, immediately set the frequency
     setClock(0);
+    // To avoid possible false configuration settings, immediately set the frequency to 1 Hz
     setFrequency(1);
   }
 
   void setClock(int32_t calibration = 0)
   {
-    uint32_t core_clock = reference_freq * (1000000000ULL-calibration) / 1000000000ULL;
-    uint64_t    scaled = core_clock;
-    for (shift = 32; shift > 0 && (scaled&0x100000000ULL) == 0; shift--)
-        scaled <<= 1;                   // Ensure that reciprocal fits in 32 bits
+    uint32_t calibrated_clock = core_clock * (1000000000ULL-calibration) / 1000000000ULL;
+#if     defined(DDS_MAX_PRECISION)
+    reciprocal = MAX_U64 / calibrated_clock;
+#else
     // The AVR gcc implementation has a 32x32->64 widening multiply.
     // This is quite accurate enough, and considerably faster than full 64x64.
-    reciprocal = (0x1ULL<<(32+shift)) / core_clock;
+    uint64_t    scaled = calibrated_clock;
+    for (shift = 32; shift > 0 && (scaled&0x100000000ULL) == 0; shift--)
+        scaled <<= 1;                   // Ensure that reciprocal fits in 32 bits
+    reciprocal = (0x1ULL<<(32+shift)) / calibrated_clock;
+#endif
   }
 
+  // Calculating deltas is expensive. You might use this infrequently and then use setDelta
   uint32_t frequencyDelta(uint32_t freq) const
   {
+#if     defined(DDS_MAX_PRECISION)
+    return (freq * reciprocal + 0x80000000UL) >> 32;
+#else
     // The reciprocal/16 is a rounding factor determined experimentally
     return ((uint64_t)freq * reciprocal + reciprocal/16) >> shift;
+#endif
   }
 
   void setFrequency(uint32_t freq)
@@ -82,13 +107,13 @@ public:
     setDelta(frequencyDelta(freq));
   }
 
-  void setDelta(uint32_t tuning_word)
+  void setDelta(uint32_t delta_word)
   {
     SPI.beginTransaction(SPISettings(SPIRate, LSBFIRST, SPI_MODE0));
-    for (int b = 0; b < 4; b++, tuning_word >>= 8)
-            SPI.transfer(tuning_word&0xFF); 
+    for (int b = 0; b < 4; b++, delta_word >>= 8)
+            SPI.transfer(delta_word&0xFF); 
     // The last byte contains configuration settings including phase:
-    SPI.transfer(0x01);
+    SPI.transfer(AD9851_ENABLE_MULTIPLIER);
 
     pulse(FQ_UDPin);                    // Transfer the 40-bit control word into the DDS core
     SPI.endTransaction();
--- README.md
+++ README.md
@@ -9,7 +9,6 @@ This is a template class for controlling the AD9851.
 
 Instantiate the AD9851 template with the appropriate parameters.
 You must provide a Reset pin and a digital pin number to be used for chip select (FQ/UD):
-The SPI SCK and MOSI pins will be used automatically.
 
     #define AD9851_RESET_PIN      3
     #define AD9851_FQ_UD_PIN      2
@@ -17,19 +16,24 @@ The SPI SCK and MOSI pins will be used automatically.
     class MyAD9851 : public AD9851<AD9851_RESET_PIN, AD9851_FQ_UD_PIN> {};
     MyAD9851 dds;
 
-The third template parameter is the core clock frequency, including
-the PLL multiplier. The AD9851 can use up to a 180MHz core clock.
+The third template parameter is the core clock frequency, after the 6x multiplier.
+Common modules use a 30MHz reference clock and so a 180MHz core clock.
+a calibration adjustment in parts-per-billion.
 
 The fourth template parameter is the SPI data rate, default 2Mb/s.
-You should not try to alter the fifth and sixth parameters, as these
-are set by the SPI driver.
+Hardware SPI means physical SCK (13) and MOSI (11) pins will be used automatically.
 
 The AD9851 reset will be applied at the time the constructor is run.
 Alternatively, you can reset the chip at any time later:
 
     dds.reset();
 
-If you have a good frequency counter, you can calibrate your AD9851.
+## Calibrating the frequencies
+
+If you have a good frequency counter, you apply a calibration for your AD9851 using setClock:
+
+    setClock(uint32_t calibration = 0);
+
 To calibrate, use a zero calibration value, set some frequency and
 measure it as accurately as you can with a frequency counter.
 Convert that value to parts-per-billion error (positive if the
@@ -41,16 +45,29 @@ should be 4320.
 You must setFrequency() after calling setClock with a new calibration,
 and recalculate any stored frequency deltas.
 
-    setClock(uint32_t calibration = 0);
-
 ## Setting the frequency
 
 The setFrequency() function tells the module what frequency to generate.
 
     dds.setFrequency(7140000UL);
 
+## Rapid frequency modulation
+
 If you have to change frequency often, use frequencyDelta() to compute
 a delta value, and apply that with setDelta():
 
     uint32_t div = dds.frequencyDelta(7140000UL);
     dds.setDelta(div);
+
+To make an FM signal, convert your centre frequency to a delta,
+and convert your 100% deviation to another delta.
+Then add or subtract a fraction of the deviation delta:
+
+    uint32_t centre_delta = dds.frequencyDelta(27245000UL);	// Citizen's band channel 25
+    uint32_t deviation_delta = dds.frequencyDelta(25000UL);	// +-25KHz deviation
+
+    void set_modulated_frequency(short d = 0)	// Range: -128..+128
+    {
+      // This deviation calculation will not overflow up to 700KHz deviation
+      dds.setDelta(centre_delta+(d*deviation_delta/128));
+    }
